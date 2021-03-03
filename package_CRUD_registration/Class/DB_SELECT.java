e71fa5c15fabff633234d84974c3d3d53548dad7
Kalebe Silva <62080789+Kalebeadv@users.noreply.github.com>
2021-03-02 22:39:20 -0300

    Update DB_SELECT.java

--- package_CRUD_registration/Class/DB_SELECT.java
+++ package_CRUD_registration/Class/DB_SELECT.java
@@ -4,80 +4,119 @@ import java.sql.ResultSet;
 
 public class DB_SELECT {
 	
-	public void DB_SELECT_INDIVIDUALS(String id_pf)
+	private DB_CONNECTION obj_db_connection = new DB_CONNECTION();
+	private Connection connection;
+	private PreparedStatement ps;
+	private ResultSet rs;
+	private String query;
+	
+	public void SELECT_BY_PRIMARY_KEY(String id_pf)
 	{
 		
-		DB_CONNECTION obj_db_connection = new DB_CONNECTION();
-		Connection connection = obj_db_connection.get_connection();
-		
-		PreparedStatement ps = null;
-		ResultSet rs = null;
+		setObj_db_connection(new DB_CONNECTION());
+		setConnection(getObj_db_connection().GETMYCONNECTION());
+		setPs(null);
+		setRs(null);
 		
 		try 
 		{
 			
-			String query = "SELECT * from individuals where ID_PF = ?";
-			ps = connection.prepareStatement(query);
-			ps.setString(1, id_pf);
-			System.out.println(ps);
+			setQuery("SELECT * from individuals where ID_PF = ?");
+			setPs(getConnection().prepareStatement(getQuery()));
+			getPs().setString(1, id_pf);
+			System.out.println(getPs());
 			
-			rs =  ps.executeQuery();
+			setRs(getPs().executeQuery());
 			
-			while(rs.next())
+			while(getRs().next())
 			{
 				
 				System.out.println("#-----------------------------------------------------------#");
-				System.out.println("ID: "+rs.getString("id_pf"));
-				System.out.println("Full name: "+rs.getString("first_name") +" "+ rs.getString("last_name"));
-				System.out.println("CPF: "+rs.getString("cpf"));
-				System.out.println("Date of birth: "+rs.getString("date_of_birth"));
-				System.out.println("Sex: "+rs.getString("sex"));
-				System.out.println("Email: "+rs.getString("email"));
-				System.out.println("Password: "+rs.getString("password"));
+				System.out.println("ID: "+ getRs().getString("id_pf"));
+				System.out.println("Full name: "+getRs().getString("first_name") +" "+ getRs().getString("last_name"));
+				System.out.println("CPF: "+getRs().getString("cpf"));
+				System.out.println("Date of birth: "+getRs().getString("date_of_birth"));
+				System.out.println("Sex: "+getRs().getString("sex"));
+				System.out.println("Email: "+getRs().getString("email"));
+				System.out.println("Password: "+getRs().getString("password"));
 				System.out.println("#-----------------------------------------------------------#");
 			}
 		} 
-		catch (Exception e) 
-		{
-			
-			System.out.println(e);
-		}
-		
-		
+		catch (Exception e) {System.out.println(e);}		
 	}
-	
-	public void DB_SELECT_LEGAL_ENTITY(String id_pj)
+
+	public void SELECT_ALL_FROM_TABLE()
 	{
-		DB_CONNECTION obj_db_connection = new DB_CONNECTION();
-		Connection connection = obj_db_connection.get_connection();
-		
-		PreparedStatement ps = null;
-		ResultSet rs = null;
+		setObj_db_connection(new DB_CONNECTION());
+		setConnection(getObj_db_connection().GETMYCONNECTION());
+		setPs(null);
+		setRs(null);
 		
 		try 
 		{
-			String query = "SELECT * FROM LEGAL_ENTITY WHERE ID_PJ = ?";
-			ps =  connection.prepareStatement(query);
-			ps.setString(1, id_pj);
-			System.out.println(ps);
-			rs = ps.executeQuery();
+			setQuery("SELECT * FROM INDIVIDUALS");
+			setPs(getConnection().prepareStatement(getQuery()));
+			setRs(getPs().executeQuery());
 			
-			while(rs.next())
+			while(getRs().next())
 			{
-				System.out.println("#-----------------------------------------------------------#");	
-				System.out.println("ID: "+rs.getString("id_pj"));
-				System.out.println("CNPJ: "+rs.getString("cnpj"));
-				System.out.println("Social reason: "+rs.getString("social_reason"));
-				System.out.println("Fancy name: "+rs.getString("fancy_name"));
-				System.out.println("Contact: "+rs.getString("contact"));
-				System.out.println("#-----------------------------------------------------------#");	
-
+				System.out.println("#-----------------------------------------------------------#");
+				System.out.println("ID: "+ getRs().getString("id_pf"));
+				System.out.println("Full name: "+getRs().getString("first_name") +" "+ getRs().getString("last_name"));
+				System.out.println("CPF: "+getRs().getString("cpf"));
+				System.out.println("Date of birth: "+getRs().getString("date_of_birth"));
+				System.out.println("Sex: "+getRs().getString("sex"));
+				System.out.println("Email: "+getRs().getString("email"));
+				System.out.println("Password: "+getRs().getString("password"));
+				System.out.println("#-----------------------------------------------------------#");
 			}
-		} 
+			
+		} catch (Exception e){System.out.println(e);}
 		
-		catch (Exception e) {
-			System.out.println(e);
-		}
 	}
+	
+	
+
+	public DB_CONNECTION getObj_db_connection() {
+		return obj_db_connection;
+	}
+
+	public void setObj_db_connection(DB_CONNECTION obj_db_connection) {
+		this.obj_db_connection = obj_db_connection;
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
+	public PreparedStatement getPs() {
+		return ps;
+	}
+
+	public void setPs(PreparedStatement ps) {
+		this.ps = ps;
+	}
+
+	public ResultSet getRs() {
+		return rs;
+	}
+
+	public void setRs(ResultSet rs) {
+		this.rs = rs;
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+	
+	
 
 }
